d56b806687b86c3ad004036a5366c8f9d353f7cb
wonsunil <seonil6352@gmail.com>
2020-12-22 14:41:18 +0900

    Feat: embody coupon DTO

--- src/main/java/com/sunil/datamodel/dto/CouponDTO.java
+++ src/main/java/com/sunil/datamodel/dto/CouponDTO.java
@@ -1,4 +1,39 @@
 package com.sunil.datamodel.dto;
 
+import com.sunil.model.Coupon;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class CouponDTO {
-}
+    private int couponId;
+    private int userId;
+    private Date issuedDate;
+    private Date endDate;
+    private int discountPrice;
+    private int discountPercentage;
+    private int used;
+
+    public CouponDTO(Coupon coupon) {
+        this.couponId = coupon.getCouponId();
+        this.userId = coupon.getUserId();
+        this.issuedDate = coupon.getIssuedDate();
+        this.endDate = coupon.getEndDate();
+        this.discountPrice = coupon.getDiscountPrice();
+        this.discountPercentage = coupon.getDiscountPercentage();
+        this.used = coupon.getUsed();
+    };
+
+    @Override
+    public String toString() {
+        return String.format(
+                "CouponDTO[couponId=%d, userId=%d, issuedDate='%s', endDate='%s', discountPrice=%d, discountPercentage=%d, used = %d]",
+                this.couponId, this.userId, this.issuedDate, this.endDate, this.discountPrice, this.discountPercentage, this.used
+        );
+    };
+};
