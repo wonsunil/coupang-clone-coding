b4f318c232c6797fcaadf1dc00549dd22131ec0a
wonsunil <seonil6352@gmail.com>
2020-12-22 13:47:57 +0900

    Feat: embody user model

--- src/main/java/com/sunil/springshoppigmall/model/User.java
+++ src/main/java/com/sunil/springshoppigmall/model/User.java
@@ -1,5 +1,48 @@
 package com.sunil.springshoppigmall.model;
 
-public class User {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String name;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String imageUrl;
+
+    @Builder
+    public User(String email, String password, String name, String phone, String imageUrl) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.imageUrl = imageUrl;
+    };
+
+    @Override
+    public String toString() {
+        return String.format(
+            "User[userId=%d, email='%s', name='%s', phone='%s', image='%s']",
+            this.userId, this.email, this.name, this.phone, this.imageUrl
+        );
+    };
 };
