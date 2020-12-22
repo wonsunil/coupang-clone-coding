8758993e40412142b9410f9ce88b418960b42788
wonsunil <seonil6352@gmail.com>
2020-12-23 01:13:09 +0900

    Feat: add constructor

--- src/main/java/com/sunil/datamodel/dto/CategoryDTO.java
+++ src/main/java/com/sunil/datamodel/dto/CategoryDTO.java
@@ -1,7 +1,13 @@
 package com.sunil.datamodel.dto;
 
+import com.sunil.model.Category;
+
 public class CategoryDTO {
     private String categoryName;
+
+    public CategoryDTO(Category category) {
+        this.categoryName = category.getCategoryName();
+    };
 
     @Override
     public String toString() {
