7e4db74dab6287a2c85c8b2da1b092bb08f71ce7
wonsunil <seonil6352@gmail.com>
2020-12-22 11:44:16 +0900

    Initial commit

---
+++ src/main/resources/webapp/WEB-INF/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>메인 페이지</title>
+</head>
+<body>
+    test
+</body>
+</html>
---
+++ src/test/java/com/sunil/springshoppigmall/SpringShoppigMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunil.springshoppigmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShoppigMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
