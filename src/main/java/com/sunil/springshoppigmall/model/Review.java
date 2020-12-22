268e6be33fcc4952271826084b048782c804c4e8
wonsunil <seonil6352@gmail.com>
2020-12-22 13:49:50 +0900

    Feat: embody review model

--- src/main/java/com/sunil/springshoppigmall/model/Review.java
+++ src/main/java/com/sunil/springshoppigmall/model/Review.java
@@ -1,5 +1,49 @@
 package com.sunil.springshoppigmall.model;
 
-public class Review {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int reviewId;
+
+    @Column(nullable = false)
+    private int userId;
+
+    @Column(nullable = false)
+    private int productId;
+
+    @Column
+    private String content;
+
+    @Column
+    private float rate;
+
+    @Column
+    private Date reviewDate;
+
+    @Builder
+    public Review(int userId, int productId, String content, float rate, Date reviewDate) {
+        this.userId = userId;
+        this.productId = productId;
+        this.content = content;
+        this.rate = rate;
+        this.reviewDate = reviewDate;
+    };
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Review[reviewId=%d, userId=%d, productId=%d, content='%s', rate='%s', reviewDate='%s']",
+                this.reviewId, this.userId, this.productId, this.content, this.rate, this.reviewDate
+        );
+    };
 };
