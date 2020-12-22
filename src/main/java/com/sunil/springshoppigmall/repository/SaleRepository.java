e068ead382a4169d578ca1941ccc5d22a03b006a
wonsunil <seonil6352@gmail.com>
2020-12-22 14:05:32 +0900

    Feat: add embodied sale repository

---
+++ src/main/java/com/sunil/springshoppigmall/repository/SaleRepository.java
@@ -0,0 +1,8 @@
+package com.sunil.springshoppigmall.repository;
+
+import com.sunil.springshoppigmall.model.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Integer> {
+
+};
