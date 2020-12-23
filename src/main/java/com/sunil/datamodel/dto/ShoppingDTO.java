00fc1350a4f9f0ae2c6b39ab8926d4d33ebbdeda
wonsunil <seonil6352@gmail.com>
2020-12-23 15:01:15 +0900

    Feat: embody shopping basket DTO

--- src/main/java/com/sunil/datamodel/dto/ShoppingDTO.java
+++ src/main/java/com/sunil/datamodel/dto/ShoppingDTO.java
@@ -1,4 +1,29 @@
 package com.sunil.datamodel.dto;
 
+import com.sunil.model.Shopping;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class ShoppingDTO {
-}
+    private int userId;
+    private int amount;
+    private int productId;
+
+    public ShoppingDTO(Shopping shopping) {
+        this.userId = shopping.getUserId();
+        this.amount = shopping.getAmount();
+        this.productId = shopping.getProductId();
+    };
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Shopping[userId=%d, amount=%d, productId=%d]",
+                this.userId, this.amount, this.productId
+        );
+    };
+};
