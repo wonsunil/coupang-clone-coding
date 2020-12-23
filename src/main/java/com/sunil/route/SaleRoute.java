177371a03556f41ee370dca40a17f8d5763da93a
wonsunil <seonil6352@gmail.com>
2020-12-24 00:37:17 +0900

    Feat: get sale by id and get sales

--- src/main/java/com/sunil/route/SaleRoute.java
+++ src/main/java/com/sunil/route/SaleRoute.java
@@ -1,9 +1,14 @@
 package com.sunil.route;
 
+import com.sunil.datamodel.dto.SaleDTO;
 import com.sunil.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/sale")
@@ -15,5 +20,13 @@ public class SaleRoute {
         this.saleService = saleService;
     };
 
+    @GetMapping("")
+    public List<SaleDTO> getSales() {
+        return this.saleService.sales();
+    };
 
+    @GetMapping("/{saleId}")
+    public SaleDTO getSaleById(@PathVariable(value = "saleId") String saleId) throws Exception {
+        return this.saleService.saleBySaleId(Integer.parseInt(saleId));
+    };
 };
--- src/main/java/com/sunil/service/SaleService.java
+++ src/main/java/com/sunil/service/SaleService.java
@@ -1,8 +1,14 @@
 package com.sunil.service;
 
+import com.sunil.datamodel.dto.SaleDTO;
+import com.sunil.model.Sale;
 import com.sunil.repository.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class SaleService {
@@ -13,4 +19,13 @@ public class SaleService {
         this.saleRepository = saleRepository;
     };
 
+    public List<SaleDTO> sales() {
+        return this.saleRepository.findAll().stream().map(SaleDTO::new).collect(Collectors.toList());
+    };
+
+    public SaleDTO saleBySaleId(int saleId) throws Exception {
+        Optional<Sale> searchedSale = this.saleRepository.findById(saleId);
+
+        return new SaleDTO(searchedSale.orElseThrow(() -> new Exception("존재하지 않는 구매 기록입니다")));
+    };
 }
