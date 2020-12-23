a655d0fc66e25d7a7f70c0a746379b665e000035
wonsunil <seonil6352@gmail.com>
2020-12-23 15:06:22 +0900

    Feat: add repository annotation

--- src/main/java/com/sunil/repository/UserRepository.java
+++ src/main/java/com/sunil/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.sunil.repository;
 
 import com.sunil.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 };
