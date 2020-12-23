f7e3e3deb61ec3f4807e1876d5be551a588bd0ec
wonsunil <seonil6352@gmail.com>
2020-12-23 10:14:22 +0900

    Feat: add get review's rate by product

--- src/main/java/com/sunil/repository/ProductRepository.java
+++ src/main/java/com/sunil/repository/ProductRepository.java
@@ -1,12 +1,17 @@
 package com.sunil.repository;
 
-import com.sunil.datamodel.dto.ProductDTO;
-import com.sunil.model.Category;
 import com.sunil.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     public List<Product> findByCategoryId(int categoryId);
+
+    @Query(value =
+            "SELECT PRODUCT_ID, SUM(RATE) as totalRate " +
+                    "FROM REVIEW WHERE PRODUCT_ID = ? GROUP BY PRODUCT_ID",
+            nativeQuery = true)
+    public Float getRateByProductId(int productId);
 };
