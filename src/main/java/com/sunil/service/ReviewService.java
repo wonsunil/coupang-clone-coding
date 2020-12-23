7ecccda51c7411570065d7e00e03e17f46a6e281
wonsunil <seonil6352@gmail.com>
2020-12-23 11:04:30 +0900

    Refactor: remove java class import

--- src/main/java/com/sunil/service/ReviewService.java
+++ src/main/java/com/sunil/service/ReviewService.java
@@ -7,7 +7,6 @@ import com.sunil.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
