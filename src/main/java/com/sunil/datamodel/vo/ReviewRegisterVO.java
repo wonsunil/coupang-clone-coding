5c33a3fc483d453617d6299649f4eace8cf5c929
wonsunil <seonil6352@gmail.com>
2020-12-23 12:13:33 +0900

    Feat: add annotation and remove @Id column

--- src/main/java/com/sunil/datamodel/vo/ReviewRegisterVO.java
+++ src/main/java/com/sunil/datamodel/vo/ReviewRegisterVO.java
@@ -1,9 +1,15 @@
 package com.sunil.datamodel.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class ReviewRegisterVO {
-    private int reviewId;
     private int userId;
     private int productId;
     private String content;
@@ -13,8 +19,8 @@ public class ReviewRegisterVO {
     @Override
     public String toString() {
         return String.format(
-                "Review[reviewId=%d, userId=%d, productId=%d, content='%s', rate='%s', reviewDate='%s']",
-                this.reviewId, this.userId, this.productId, this.content, this.rate, this.reviewDate
+                "Review[userId=%d, productId=%d, content='%s', rate='%s', reviewDate='%s']",
+                this.userId, this.productId, this.content, this.rate, this.reviewDate
         );
     };
 };
