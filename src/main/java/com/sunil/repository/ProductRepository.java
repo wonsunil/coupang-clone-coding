113dc6f1ae3eb8f73fbcbebf37ca24c75aa2cd8d
wonsunil <seonil6352@gmail.com>
2020-12-23 09:51:24 +0900

    Feat: add products by category

--- src/main/java/com/sunil/repository/ProductRepository.java
+++ src/main/java/com/sunil/repository/ProductRepository.java
@@ -1,8 +1,12 @@
 package com.sunil.repository;
 
+import com.sunil.datamodel.dto.ProductDTO;
+import com.sunil.model.Category;
 import com.sunil.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+import java.util.List;
 
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    public List<Product> findByCategoryId(int categoryId);
 };
