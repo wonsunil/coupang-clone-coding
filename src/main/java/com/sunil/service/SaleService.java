5d9e370e00ebd77e9d9924fd82328a2332f84356
wonsunil <seonil6352@gmail.com>
2020-12-24 00:32:01 +0900

    Feat: autowired sale repository

--- src/main/java/com/sunil/service/SaleService.java
+++ src/main/java/com/sunil/service/SaleService.java
@@ -1,7 +1,16 @@
 package com.sunil.service;
 
+import com.sunil.repository.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SaleService {
+    private final SaleRepository saleRepository;
+
+    @Autowired
+    public SaleService(SaleRepository saleRepository) {
+        this.saleRepository = saleRepository;
+    };
+
 }
