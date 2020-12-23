6d6190d355779516d9a5fd102e6888ef6c0f111a
wonsunil <seonil6352@gmail.com>
2020-12-24 00:31:35 +0900

    Feat: autowired sale service

--- src/main/java/com/sunil/route/SaleRoute.java
+++ src/main/java/com/sunil/route/SaleRoute.java
@@ -1,10 +1,19 @@
 package com.sunil.route;
 
+import com.sunil.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sale")
 public class SaleRoute {
+    private final SaleService saleService;
+
+    @Autowired
+    public SaleRoute(SaleService saleService) {
+        this.saleService = saleService;
+    };
+
 
 };
