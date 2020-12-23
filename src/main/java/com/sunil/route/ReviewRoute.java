f2ab34bfd6c48948fe03e768ce0d436404a6051b
wonsunil <seonil6352@gmail.com>
2020-12-23 11:04:42 +0900

    Feat: embody review route

--- src/main/java/com/sunil/route/ReviewRoute.java
+++ src/main/java/com/sunil/route/ReviewRoute.java
@@ -1,10 +1,40 @@
 package com.sunil.route;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sunil.datamodel.dto.ReviewDTO;
+import com.sunil.datamodel.vo.ReviewRegisterVO;
+import com.sunil.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/review")
 public class ReviewRoute {
+    private final ReviewService reviewService;
 
+    @Autowired
+    public ReviewRoute(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    };
+
+    @GetMapping("")
+    public List<ReviewDTO> getReviews() {
+        return this.reviewService.reviews();
+    };
+
+    @GetMapping("/{reviewId}")
+    public ReviewDTO getReviewByReviewId(@PathVariable(value = "reviewId") String reviewId) throws Exception{
+        return this.reviewService.reviewByReviewId(Integer.parseInt(reviewId));
+    };
+
+    @PostMapping
+    public int createReview(ReviewRegisterVO review) {
+        return this.reviewService.createReview(review);
+    };
+
+    @DeleteMapping("/{reviewId}")
+    public void deleteReview(@PathVariable(value = "reviewId") String reviewId) {
+        this.reviewService.deleteReview(Integer.parseInt(reviewId));
+    };
 };
